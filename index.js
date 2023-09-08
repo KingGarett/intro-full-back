7612024f74b2a0af118a29bae32d709a385e8681
kinggarett <carlosjavier094@gmail.com>
2023-09-08 01:49:03 -0300

    primer deploy pero con errores

--- index.js
+++ index.js
@@ -1,17 +1,100 @@
-require("dotenv").config()
-const http = require('http')
+const express = require("express");
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const requestController = () => [
-    console.log('ha llegagdo una peticion')
-]
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('Conexion exitodo con la DDBB'))
+    .catch(err => console.log('Ocurrio un error al tratar de conectar con la DDBB' + err))
 
-const server = http.createServer((req, res) => {
-    res.end('terminando la respuesta')
-    requestController()
+const taskSchema = new Schema({
+    name: String,
+    done: Boolean
 })
 
-const PORT = process.env.PORT
+const Task = mongoose.model('Task', taskSchema, 'Tasks')
 
-server.listen(PORT, () => {
-    console.log('corriendo en puerto:' + 4000)
+const app = express()
+
+// Middleware para servir archivos estaticos
+app.use(express.static('./public'))
+
+// Middleware para extraer los chunks del post que me envian mediante fetch
+
+app.use(express.json())
+
+// Creando los middleware
+
+app.use((req, res, next) => {
+    console.log('no estoy especificando ninguna ruta')
+    next()
+})
+
+// Rutas del proyecto
+
+app.get('/users', (req, res) => {
+    res.json([{ name: 'jorge' }, { name: 'lucia' }])
+})
+
+app.get('/api/tasks', (req, res) => {
+    Task.find()
+        .then((tasks) => {
+            res.status(200).json({ ok: true, data: tasks })
+        })
+        .catch((err) => {
+            res.status(400).json({ ok: false, messaje: 'Ocurrio un fallo al traer los archivos' + err })
+        })
+})
+
+app.post('/api/tasks', (req, res) => {
+    const body = req.body
+    console.log(body)
+    Task.create({
+        name: body.text,
+        done: false
+    }).then((createTask) => {
+        res.status(201).json({
+            ok: true,
+            messaje: 'tarea creada con exito',
+            data: createTask
+        })
+    }).catch((err) => {
+        res.status(400).json({
+            ok: false,
+            messaje: 'Ocurrio un erro al crear la tarea' + { err }
+        })
+    })
+})
+
+app.put('/api/tasks/:id', (req, res) => {
+    const id = req.params.id
+    const newText = req.body.text
+    Task.findByIdAndUpdate(id, {
+        name: newText
+    })
+        .then(updateTask =>
+            res.status(201).json({
+                ok: true,
+                messaje: 'Tarea actualizada con exito',
+                data: updateTask
+            }))
+        .catch(err => res.status(400).json({
+            ok: false,
+            messaje: 'Error al actualizar ' + err
+        }))
+})
+
+
+app.delete('/api/tasks/:id', (req, res) => {
+    const id = req.params.id
+    Task.findByIdAndRemove(id).then((deletedTask) => {
+        res.status(200).json({ ok: true, data: deletedTask })
+    }).catch(err =>
+        res.status(400).json({ ok: false, messaje: 'Ocurrio un error al eliminar la tarea ' + err }))
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('escuchando en puerto' + PORT)
 })
---
+++ public/index.js
@@ -0,0 +1,91 @@
+const input = document.querySelector('#task-name')
+const getUsersBtn = document.getElementById('get-users')
+const createEditTaskBtn = document.getElementById('create-task')
+let main = document.querySelector('main')
+
+const baseBackendUrl = 'http://localhost:4000/api'
+
+getUsersBtn.addEventListener('click', async () => {
+
+    console.log('obteniendo usuarios');
+    await fetch('http://localhost:4000/users')
+        .then(response => response.json())
+        .then(data => {
+            main.innerHTML = data.map(user => `<li style='list-style:none'>Nombre: ${user.name}</li>`)
+        })
+
+})
+
+let TASK__TO_EDIT = null
+
+// OBTENIENDO LAS TAREAS
+const getTask = () => {
+    main.innerHTML = null
+    fetch(`${baseBackendUrl}/tasks`)
+        .then(res => res.json())
+        .then(data => {
+
+            data.data.forEach(element => {
+                const div = document.createElement('div')
+                const taskParraf = document.createElement('p')
+                taskParraf.addEventListener('click', () => {
+                    input.value = taskParraf.innerText
+                    createEditTaskBtn.innerText = 'Editar Task'
+                    TASK__TO_EDIT = element
+                    console.log(TASK__TO_EDIT);
+                })
+
+                const deleteTaskBtn = document.createElement('button')
+                deleteTaskBtn.classList.add('get-task')
+                deleteTaskBtn.innerText = 'Borrar'
+                deleteTaskBtn.setAttribute('id', element._id)
+                deleteTaskBtn.addEventListener('click', (e) => {
+                    const taskId = e.target.id
+                    deleteTaskBtn.innerHTML = '...'
+                    fetch(`${baseBackendUrl}/tasks/${taskId}`, {
+                        method: 'DELETE'
+                    })
+                        .then(() => {
+                            const divTask = deleteTaskBtn.parentElement
+                            divTask.remove()
+                            input.focus()
+                        })
+                })
+
+                div.appendChild(taskParraf)
+                div.appendChild(deleteTaskBtn)
+                main.appendChild(div)
+
+                taskParraf.innerText = element.name
+            })
+
+        })
+        .catch(err => console.log('Ocurrio un error' + err))
+}
+
+createEditTaskBtn.addEventListener('click', () => {
+    const creating = !TASK__TO_EDIT
+    const path = creating ? 'tasks' : `tasks/${TASK__TO_EDIT._id}`
+    const method = creating ? "POST" : "PUT"
+
+    fetch(`${baseBackendUrl}/${path}`, {
+        method: `${method}`,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ text: input.value })
+    }).then(() => {
+        input.value = ''
+        createEditTaskBtn.innerText = 'Crear Tarea'
+        input.focus()
+        getTask()
+    })
+})
+
+// const suma = (especie1, especie2, ...animales) => {
+//     let animalesGroup = ''
+//     for (const animal of animales) {
+//         animalesGroup += animal
+//     }
+//     console.log(especie1, especie2)
+// }
+
+// suma('aligartor', 'pollo', 'serpiente', 'lombriz')
